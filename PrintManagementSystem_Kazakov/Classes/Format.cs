e384e62e9a842c2876db9eff223e26d2deeff7a8
qwelord <secondofall22@rambler.ru>
2025-11-17 19:58:55 +0500

    Реализованные классы.Казаков

---
+++ PrintManagementSystem_Kazakov/Classes/Format.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace PrintManagementSystem_Kazakov.Classes
+{
+    public class Format
+    {
+        public int id { get; set; }
+        public string format{ get; set; }
+        public string description { get; set; }
+        public Format(int _id, string _format, string _description) { 
+            this.id = _id; this.format = _format; this.description = _description;
+        }
+
+        public static List<Format> AllFormats()
+        {
+            return new List<Format>
+            {
+                new Format(1, "A4", ""),
+                new Format(2, "A3", ""),
+                new Format(3, "A2", ""),
+                new Format(4, "A1", "")
+            };
+        }
+    }
+}
---
+++ PrintManagementSystem_Kazakov/Classes/TypeOpertation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManagementSystem_Kazakov.Classes
+{
+    public class TypeOpertation
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public TypeOpertation(int _id, string _name, string _description) { 
+            this.id = _id; this.name = _name; this.description = _description;
+        }
+        public static List<TypeOpertation> AllTypeOperation()
+        {
+            return new List<TypeOpertation>()
+            {
+                new TypeOpertation(1, "Печать", ""),
+                new TypeOpertation(2, "Копия", ""),
+                new TypeOpertation(3, "Скан", ""),
+                new TypeOpertation(4, "Ризограф", "")
+            };
+        }
+    }
+}
