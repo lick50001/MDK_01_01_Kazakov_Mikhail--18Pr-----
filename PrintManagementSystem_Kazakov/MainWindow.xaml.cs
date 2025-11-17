58cba4a9ac757636b6f26f0b74da227bd8d0b1c8
qwelord <secondofall22@rambler.ru>
2025-11-17 19:46:49 +0500

    Разработанное главное окно. Казаков

--- PrintManagementSystem_Kazakov/MainWindow.xaml.cs
+++ PrintManagementSystem_Kazakov/MainWindow.xaml.cs
@@ -1,28 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PrintManagementSystem_Kazakov
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DeleteOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectedType(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SelectedFormat(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void textBoxCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void AddOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ColorsChange(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
